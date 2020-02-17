5ebe83349cc06b5ea0cef0e257174e718eeca17a
ivangonzalez07 <igonzalezga19dw31@ikzubirimanteo.com>
2020-02-17 19:06:16 +0100

    primer comit

---
+++ holamundo.java
@@ -0,0 +1,13 @@
+/*
+  * Ejemplo HolaMundo 
+  * Imprime el mensaje "Hola, Mundo!" 
+*/
+    
+public class HolaMundo {
+    public staitc void main( String[] args ){
+       
+        System.out.println("Hola, Mundo desde Java!")
+        
+    }
+
+}
